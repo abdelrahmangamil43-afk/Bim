3723f51d5054c60763626b97dac7330ec8a6f4a7
Abdelrahman <abdelrahmangamil43@gmail.com>
2026-09-18 23:23:55 +0300

    feat: - add BMI calculator widget - add height selector - add weight selector - add gender selection

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bmi/widgets/gender.dart';
+import 'package:bmi/widgets/user.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,7 +14,6 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff1C2135),
-
       appBar: AppBar(
         title: Text(
           'BMI Calculator',
@@ -23,29 +23,97 @@ class _HomeScreenState extends State<HomeScreen> {
             fontWeight: .w600,
           ),
         ),
-
         centerTitle: true,
         backgroundColor: Color(0xff24263B),
       ),
-
-      body: Column(
-        children: [
-          SizedBox(height: 20),
-          Row(
-            spacing: 10,
-            children: [
-              Genderwidget(image: "assets/images/male.png", title: "Male"),
-              Genderwidget(image: "assets/images/female.png", title: "Female"),
-            ],
-          ),
-          Expanded(child: Container()),
-
-          Expanded(child: Row(children: [
-            
-                    ],)),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          spacing: 25,
+          children: [
+            SizedBox(height: 20),
+            Row(
+              spacing: 10,
+              children: [
+                Genderwidget(image: "assets/images/malee.png", title: "Male"),
+                Genderwidget(
+                  image: "assets/images/femalee.png",
+                  title: "Female",
+                ),
+              ],
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xff333244),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  mainAxisAlignment: .spaceEvenly,
+                  children: [
+                    Text(
+                      "Height",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: .w400,
+                        color: Color(0xff8B8C9E),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: .center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "170",
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: .w600,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "cm",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: .w400,
+                            color: Color(0xff8B8C9E),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Slider(
+                      activeColor: Color(0xffE83D67),
+                      min: 50,
+                      max: 250,
+                      value: 170,
+                      onChanged: (value) {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                spacing: 10,
+                children: [
+                  Userwidget(
+                    title: "Weight",
+                    value: 60,
+                    add: () {},
+                    remove: () {},
+                  ),
+                  Userwidget(
+                    title: "Age",
+                    value: 26,
+                    add: () {},
+                    remove: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-
       bottomNavigationBar: MaterialButton(
         onPressed: () {},
         color: Color(0xffE83D67),
---
+++ lib/widgets/user.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class Userwidget extends StatelessWidget {
+  const Userwidget({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.add,
+    required this.remove,
+  });
+  final String title;
+  final int value;
+  final void Function() add;
+  final void Function() remove;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color(0xff24263B),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          mainAxisAlignment: .spaceEvenly,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: .w400,
+                color: Color(0xff8B8C9E),
+              ),
+            ),
+            Text(
+              value.toString(),
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: .bold,
+                color: Color(0xffFFFFFF),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: .spaceAround,
+              children: [
+                IconButton(
+                  style: IconButton.styleFrom(
+                    backgroundColor: Color(0xff8B8C9E),
+                    elevation: 5,
+                  ),
+                  onPressed: add,
+                  icon: Icon(Icons.add, color: Colors.white, size: 35),
+                ),
+                IconButton(
+                  style: IconButton.styleFrom(
+                    backgroundColor: Color(0xff8B8C9E),
+                    elevation: 5,
+                  ),
+                  onPressed: remove,
+                  icon: Icon(Icons.remove, color: Colors.white, size: 35),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
